acae659e8b92a97cf87853f01f19a583d9abe9cb
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-10-27 13:59:06 +0000

    Open Config Vars (bad practice)

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,8 +3,9 @@
 /*Adding api key in react allows anyone to view it 
 for a real app this would be better served with the API Key existing 
 only in the backend */
-export const apiKey = process.env.REACT_APP_API_KEY
-export const hotelId = process.env.REACT_APP_HOTEL_ID
+
+export const apiKey = "d2fbff2e3d1204e9522ba1f187199ab5c540d5eb4cfa7a7602d652cb4395faab243734fa38f96feffbc105ce207d32ef"
+export const hotelId = "e8bd41b0-b98e-4a78-ad14-c7778a00ab00"
 
 export const baseURI = "https://api.getimpala.com/v2/hotel"
 
