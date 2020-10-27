76f38fad38235f6adcdb1f9620020aa6cfbb8cef
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-10-27 12:59:26 +0000

    debuging .env

--- src/services/getAreas.js
+++ src/services/getAreas.js
@@ -1,15 +1,10 @@
 import axios from 'axios'
+import {apiKey,hotelId,baseURI} from '../utils/config'
 
 const getAreas = async () =>{
-    /*Adding api key in react allows anyone to view it 
-    for a real app this would be better served with the API Key existing 
-    only in the backend */
-    const apiKey = process.env.REACT_APP_API_KEY
-    const hotelId = process.env.REACT_APP_HOTEL_ID
-    console.log("key is: ", apiKey)
-    console.log("hotel ID is: ",hotelId )
-    const URI = `https://api.getimpala.com/v2/hotel/${hotelId}/areas`
 
+    const URI = `${baseURI}/${hotelId}/areas`
+    
     const config = {
         headers: {
             Authorization: 'Bearer ' + apiKey 
--- src/utils/config.js
+++ src/utils/config.js
@@ -7,3 +7,7 @@ export const apiKey = process.env.REACT_APP_API_KEY
 export const hotelId = process.env.REACT_APP_HOTEL_ID
 
 export const baseURI = "https://api.getimpala.com/v2/hotel"
+
+
+console.log("key is: ", apiKey)
+console.log("hotel ID is: ",hotelId )
